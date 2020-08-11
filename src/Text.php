0a296df822f35607ee3b1bb0ac5edce78a746603
Sampan Verma <sampan.developer@gmail.com>
2020-08-11 15:17:19 +0530

    Added bootstrap for unit tests.

--- src/Text.php
+++ src/Text.php
@@ -18,257 +18,311 @@ class Text
     public function after()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function afterLast()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function append()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function ascii()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function baseName()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function before()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function beforeLast()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function between()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function camel()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function contains()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function containsAll()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function dirName()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function endsWith()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function finish()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function is()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function isAscii()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function isEmpty()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function isUuid()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function isNotEmpty()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function kebab()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function length()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function limit()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function lower()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function ltrim()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function match()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function matchAll()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function plural()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function prepend()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function random()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function replace()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function replaceArray()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function replaceFirst()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function replaceLast()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function replaceMatches()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function rtrim()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function singular()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function slug()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function split()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function snake()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function start()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function startsWith()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function studly()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function substr()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function title()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
-    public function trim()
+    public function trim($charlist = " \t\n\r\0\x0B")
     {
         $data = $this->getData();
+
+        $data = trim($data, $charlist);
+
         return $this->setData($data);
     }
     public function ucfirst()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function upper()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function uuid()
     {
         throw new BadMethodCallException("Function will be added soon.");
+
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function when()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function whenEmpty()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
     public function words()
     {
         $data = $this->getData();
+
         return $this->setData($data);
     }
 }
--- tests/TextTest.php
+++ tests/TextTest.php
@@ -17,8 +17,208 @@ class TextTest extends TestCase
         $name = $firstName . " " . $lastName;
         $this->assertEquals($name, "Hello World");
     }
-    public function testExample()
+    public function testAfter()
     {
-        $this->assertTrue(true);
+        return $this->assertTrue(false);
+    }
+    public function testAfterLast()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testAppend()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testAscii()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testBaseName()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testBefore()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testBeforeLast()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testBetween()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testCamel()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testContains()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testContainsAll()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testDirName()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testEndsWith()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testFinish()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testIs()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testIsAscii()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testIsEmptyFunction()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testIsUuid()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testIsNotEmpty()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testKebab()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testLength()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testLimit()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testLower()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testLtrim()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testMatch()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testMatchAll()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testPlural()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testPrepend()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testRandom()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testReplace()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testReplaceArray()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testReplaceFirst()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testReplaceLast()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testReplaceMatches()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testRtrim()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testSingular()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testSlug()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testSplit()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testSnake()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testStart()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testStartsWith()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testStudly()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testSubstr()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testTitle()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testTrim()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testUcfirst()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testUpper()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testUuid()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testWhen()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testWhenEmpty()
+    {
+        return $this->assertTrue(false);
+    }
+    public function testWords()
+    {
+        return $this->assertTrue(false);
     }
 }
